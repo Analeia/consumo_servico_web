9883dc965249ee3993543f99ea69057f883a67b2
Ana <analeiarr@gmail.com>
2021-02-04 23:18:11 -0400

    commit do projeto consumo de serviço web

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:consumo_servicos/home.dart';
+import 'package:flutter/material.dart';
+import 'package:consumo_servicos/home.dart';
+
+main() {
+  runApp(MaterialApp(
+    home: Home(),
+  ));
+}
